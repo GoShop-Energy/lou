a7a76f56d91b9a8d5c39ca145109c3798ebf9383
assia <johndoe@example.com>
2024-02-21 09:25:58 +0200

    [IMP]gse_inventory_reports: Enhancing Inventory Performance Reports

---
+++ gse_bom/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Gse BOM",
+    "summary": """
+        Customisation of Field Service module for GoShop Energy""",
+    "description": """
+    """,
+    "author": "Magana Mwinja Asiati",
+    "website": "https://github.com/GoShop-Energy/field-service",
+    "category": "Customizations",
+    "version": "0.1.8.7",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "delivery",
+        "industry_fsm",
+        "industry_fsm_report",
+        "product",
+        "project",
+        "sale_management",
+        "sale_project",
+        "stock",
+        "stock_enterprise",
+        "mrp",
+    ],
+    "data": [ ],
+    "images": [ ],
+    "assets": {"web.assets_backend": []},
+}
