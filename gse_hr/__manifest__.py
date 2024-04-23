a4c424701a992680feafe10556a5a1c5aba4f181
benjamin <benkisenge03@gmail.com>
2024-04-23 10:10:23 +0200

    [IMP] res.users: add job position

---
+++ gse_hr/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "gse_hr",
+    'summary': """
+        Customizations pour Goshop Energy""", 
+    'author': "Benjamin Kisenge",
+    'website': "http://www.benkis.com", 
+    'category': 'Customizations',
+    'version': '0.1.8.6',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'hr'
+    ], 
+    'data': [
+        'views/res_users.xml',
+    ]
+}
