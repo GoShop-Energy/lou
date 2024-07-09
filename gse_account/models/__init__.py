9f7d2f8376dca67b0bd4a04d381d60ce1c543449
Sébastien Bühl <buhlsebastien@gmail.com>
2024-07-09 10:44:15 +0200

    [IMP] Restriction of Tax Application to Selected Journals

--- gse_account/models/__init__.py
+++ gse_account/models/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
-from . import res_config_settings
-from . import company
+from . import account_journal
 from . import account_move
+from . import account_tax
+from . import company
+from . import res_config_settings
