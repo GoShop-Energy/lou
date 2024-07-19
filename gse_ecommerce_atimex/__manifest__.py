bd78b26118b21c3538e5f4bfbd0c2d0de5ffd90b
assia <johndoe@example.com>
2024-07-19 10:17:28 +0200

    [IMP] improve navbar design

---
+++ gse_ecommerce_atimex/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "atimex",
+    "category": "Customizations",
+    "version": "1.0",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+        "sale",
+        "website",
+    ],
+    "data": [
+        "views/website_data.xml",
+    ],
+}
