31db24f2ac8f5d84971fd05a6b109493a5aecda3
Sébastien Bühl <buhlsebastien@gmail.com>
2024-08-01 11:48:26 +0200

    [MIG][17.0] credit_management

--- sod_sale_payment_method/__manifest__.py
+++ sod_sale_payment_method/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2019-2022 Sodexis
+# Copyright 2019-2023 Sodexis
 # License OPL-1 (See LICENSE file for full copyright and licensing details).
 
 {
     "name": "Sale Payment Method ",
-    "summary": """Adds the payment method on the Customer and the Sale Order. """,
+    "summary": """Adds the payment method on the Customer and the Sale Order.""",
     "version": "17.0.1.0.0",
     "category": "Sale",
     "website": "https://sodexis.com/",
@@ -19,5 +19,5 @@
         "views/res_partner_view.xml",
         "views/sale_view.xml",
     ],
-    "images": ["images/main_screenshot.png"],
+    "images": ["images/main_screenshot.jpg"],
 }
