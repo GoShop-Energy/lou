4c512fb86b4e4fd5a0dd2191555f98f62627dbc5
Sébastien Bühl <buhlsebastien@gmail.com>
2024-08-08 10:05:12 +0200

    [MIG][17.0] Cloud_storage

--- cloud_storage/cloud_storage_testing/__manifest__.py
+++ cloud_storage/cloud_storage_testing/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Cloud Storage Testing",
     "summary": """Additional testing module, to be used with cloud storage emulators""",
     "category": "Technical Settings",
-    "version": "17.0.0",
+    "version": "17.0.0.1",
     "depends": ["cloud_storage"],
     "data": [
     ],
