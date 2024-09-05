a5358bf1169f5c5716a3357df6c7f314ce09ba6d
shortcut1337 <really_mad@hotmail.com>
2024-09-06 00:41:16 +0200

    migrate js custo

--- gse_website_sale_stock/static/src/js/variant_mixin.js
+++ gse_website_sale_stock/static/src/js/variant_mixin.js
@@ -1,76 +1,74 @@
-odoo.define('gse_website_sale_stock.VariantMixin', function (require) {
-    'use strict';
-    
-    const publicWidget = require('web.public.widget');
-    const core = require('web.core');
-    const Dialog = require('web.Dialog');
-    const QWeb = core.qweb;
-    
-    require('website_sale.website_sale');
-    
-    publicWidget.registry.WebsiteSale.include({
-        events: Object.assign({}, publicWidget.registry.WebsiteSale.prototype.events, {
-            'click .gse_change_company': '_onChangeCompanyClick',
-            'click .gse_change_warehouse': '_onChangeWarehouseClick',
-        }),
-        _onChangeWarehouseClick(ev) {
-            const gseWhQty = $(ev.target).data('gse-wh-qty');
-            const dialog = new Dialog(this, {
-                size: 'medium',
-                title: 'Select another warehouse',
-                technical: false,
-                $content: QWeb.render('gse_website_sale_stock.popup_content_warehouse_select', {
-                    'qty': gseWhQty,
-                }),
-                buttons: [
-                    {
-                        text: "Save",
-                        classes: 'btn-primary',
-                        close: true,
-                        click: function () {
-                            const selectedWarehouse = dialog.$el.find('input[name="gse_select_warehouse"]:checked');
-                            const whId = selectedWarehouse[0].value;
-                            const search = $.deparam(window.location.search.substring(1));
-                            search['gse_forced_warehouse_id'] = whId;
-                            window.location.search = $.param(search);
-                        },
-                    },
-                    {
-                        text: "Cancel",
-                        close: true,
-                    },
-                ],
-            }).open();
-        },
-    
-        _onChangeCompanyClick(ev) {
-            const gseWebsiteCompanies = $(ev.target).data('gse-website-companies');
-            const dialog = new Dialog(this, {
-                size: 'medium',
-                title: 'Select another country',
-                technical: false,
-                $content: QWeb.render('gse_website_sale_stock.popup_content_company_select', {
-                    'companies': gseWebsiteCompanies,
-                }),
-                buttons: [
-                    {
-                        text: "Save",
-                        classes: 'btn-primary',
-                        close: true,
-                        click: function () {
-                            const selectedWarehouse = dialog.$el.find('input[name="gse_select_company"]:checked');
-                            const companyDomain = selectedWarehouse[0].value;
-                            let currentUrl = new URL(window.location);
-                            window.location = currentUrl.href.replace(currentUrl.origin, companyDomain);
-                        },
-                    },
-                    {
-                        text: "Cancel",
-                        close: true,
-                    },
-                ],
-            }).open();
-        },
-    });
-    
-    });
+/** @odoo-module **/
+
+import publicWidget from "@web/legacy/js/public/public_widget";
+import { Dialog } from "@web/core/dialog/dialog";
+const { Component } = owl;
+
+export class WarehouseSelectDialog extends Component {
+    setup() {
+        this.size = 'md';
+        this.title = 'Select another warehouse';
+    }
+
+    _onConfirm() {
+        const selectedWarehouse = $('input[name="gse_select_warehouse"]:checked');
+        const whId = selectedWarehouse[0].value;
+        const search = new URL(window.location).searchParams;
+        search.set("gse_forced_warehouse_id", whId);
+        window.location.search = search.toString();
+        this.props.close();
+    }
+}
+
+WarehouseSelectDialog.template = 'gse_website_sale_stock.popup_content_warehouse_select_dialog';
+WarehouseSelectDialog.props = {
+    qty: { type: Object },
+    close: { type: Function },
+};
+WarehouseSelectDialog.components = { Dialog };
+
+
+export class CompanySelectDialog extends Component {
+    setup() {
+        this.size = 'md';
+        this.title = 'Select another country';
+    }
+
+    _onConfirm() {
+        const selectedWarehouse = $('input[name="gse_select_company"]:checked');
+        const companyDomain = selectedWarehouse[0].value;
+        let currentUrl = new URL(window.location);
+        window.location = currentUrl.href.replace(currentUrl.origin, companyDomain);
+    }
+}
+
+CompanySelectDialog.template = 'gse_website_sale_stock.popup_content_company_select_dialog';
+CompanySelectDialog.props = {
+    companies: { type: Object },
+    close: { type: Function },
+};
+CompanySelectDialog.components = { Dialog };
+
+publicWidget.registry.WebsiteSale.include({
+    events: Object.assign({}, publicWidget.registry.WebsiteSale.prototype.events, {
+        'click .gse_change_company': '_onChangeCompanyClick',
+        'click .gse_change_warehouse': '_onChangeWarehouseClick',
+    }),
+    init() {
+        this._super(...arguments);
+        this.dialogService = this.bindService("dialog");
+    },
+    _onChangeWarehouseClick(ev) {
+        const gseWhQty = $(ev.target).data('gse-wh-qty');
+        this.call("dialog", "add", WarehouseSelectDialog, {
+            'qty': gseWhQty,
+        });
+    },
+
+    _onChangeCompanyClick(ev) {
+        const gseWebsiteCompanies = $(ev.target).data('gse-website-companies');
+        this.call("dialog", "add", CompanySelectDialog, {
+            'companies': gseWebsiteCompanies,
+        });
+    },
+});
