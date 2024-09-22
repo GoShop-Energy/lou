d7217e1073af0849af3b00eed57843f94585527a
Sébastien Bühl <buhlsebastien@gmail.com>
2024-09-22 17:26:58 +0200

    [FIX] cloud_storage: Ensure res_ids is always a list

--- cloud_storage/cloud_storage/models/ir_actions_report.py
+++ cloud_storage/cloud_storage/models/ir_actions_report.py
@@ -4,16 +4,16 @@ class IrActionsReport(models.Model):
     _inherit = 'ir.actions.report'
 
     def _render_qweb_pdf(self, report_ref, res_ids=None, data=None):
-        # Assurez-vous que res_ids est une liste
-        if not isinstance(res_ids, list):
-            res_ids = [res_ids]
-        
-        # move attachments for the given records after pdf creation
-        # only not yet cloud attachments will be moved
         res = super()._render_qweb_pdf(report_ref, res_ids, data)
+        if res_ids is None:
+            res_ids = []
+        elif not isinstance(res_ids, (list, tuple)):
+            res_ids = [res_ids]
         attachments = self.env['ir.attachment'].search([
             ('res_model', '=', self._get_report(report_ref).model),
             ('res_id', 'in', res_ids),
         ])
         attachments.move_to_cloud()
+        
         return res
+        
