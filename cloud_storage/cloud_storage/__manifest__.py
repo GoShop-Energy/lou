fc8bad3975dc8908f791b4f02c7e516c614ba258
assia <johndoe@example.com>
2024-09-29 16:40:41 +0200

    Metric - Expected Delivery Date and Real delivery date

--- cloud_storage/cloud_storage/__manifest__.py
+++ cloud_storage/cloud_storage/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Cloud Storage",
     "summary": """Store chatter attachments in the cloud (Google or Azure)""",
     "category": "Technical Settings",
-    "version": "1.1.0",
+    "version": "17.0.1.0.0",
     "depends": ["base"],
     "data": [
         "views/settings.xml",
--- gse_delivery_date/__manifest__.py
+++ gse_delivery_date/__manifest__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "GSE Analytic",
+    "name": "GSE delivery date",
     "category": "Customizations",
     "version": "17.0.1.0.0",
     "license": "LGPL-3",
     "depends": [
-        "account",
         "sale",
     ],
-    "data": [],
+    "data": [
+        "views/sale_order.xml",
+    ],
     "assets": {"web.assets_backend": []},
 }
